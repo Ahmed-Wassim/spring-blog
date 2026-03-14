1852fa7103bd5e7ff122e688c820f25e88183890
Ahmed Wassim <ahmedwassim317@gmail.com>
2026-03-15 01:11:03 +0200

    define user post relationship

--- src/main/java/com/wassim/blog/domain/entities/User.java
+++ src/main/java/com/wassim/blog/domain/entities/User.java
@@ -1,13 +1,16 @@
 package com.wassim.blog.domain.entities;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -37,6 +40,9 @@ public class User {
 
     @Column(nullable = false)
     private String name;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts;
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
