fe4a98d27e87c4b75445219ce6472d43e083ef31
Ahmed Wassim <ahmedwassim317@gmail.com>
2026-03-15 01:13:07 +0200

    define category post relationship

--- src/main/java/com/wassim/blog/domain/entities/Category.java
+++ src/main/java/com/wassim/blog/domain/entities/Category.java
@@ -1,5 +1,6 @@
 package com.wassim.blog.domain.entities;
 
+import java.util.List;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
@@ -7,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,4 +32,6 @@ public class Category {
     @Column(nullable = false, unique = true)
     private String name;
 
+    @OneToMany(mappedBy = "category")
+    private List<Post> posts;
 }
